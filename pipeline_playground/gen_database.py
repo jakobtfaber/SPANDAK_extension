e1bc584886962d8f201fa3b0f5252f2a801a2998
Jakob Faber <jakobtfaber@gmail.com>
2020-07-03 15:32:19 -0400

    Added database generator and spandak runner scripts.

---
+++ pipeline_playground/gen_database.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import csv 
+
+fil_paths = ['/mnt_blpd9/datax/incoming/spliced_guppi_57991_49905_DIAG_FRB121102_0011.gpuspec.0001.8.4chan.fil',
+'/mnt_blpd9/datax/incoming/spliced_guppi_57991_51723_DIAG_FRB121102_0012.gpuspec.0001.8.4chan.fil',
+'/mnt_blpd9/datax/incoming/spliced_guppi_57991_53535_DIAG_FRB121102_0013.gpuspec.0001.8.4chan.fil',
+'/mnt_blpd9/datax/incoming/spliced_guppi_57991_55354_DIAG_FRB121102_0014.gpuspec.0001.8.4chan.fil',
+'/mnt_blpd9/datax/incoming/spliced_guppi_57991_57166_DIAG_FRB121102_0015.gpuspec.0001.8.4chan.fil',
+'/mnt_blpd9/datax/incoming/spliced_guppi_57991_58976_DIAG_FRB121102_0016.gpuspec.0001.8.4chan.fil',
+'/mnt_blpd9/datax/incoming/spliced_guppi_57991_60787_DIAG_FRB121102_0017.gpuspec.0001.8.4chan.fil',
+'/mnt_blpd9/datax/incoming/spliced_guppi_57991_62598_DIAG_FRB121102_0018.gpuspec.0001.8.4chan.fil',
+'/mnt_blpd9/datax/incoming/spliced_guppi_57991_64409_DIAG_FRB121102_0019.gpuspec.0001.8.4chan.fil']
+raw_paths = ['/mnt_blpd6/datax2/FRB121102/BLP00', '/mnt_blpd6/datax2/FRB121102/BLP01', \
+'/mnt_blpd6/datax2/FRB121102/BLP02', '/mnt_blpd6/datax2/FRB121102/BLP03', \
+'/mnt_blpd6/datax2/FRB121102/BLP04', '/mnt_blpd6/datax2/FRB121102/BLP05', \
+'/mnt_blpd6/datax2/FRB121102/BLP06', '/mnt_blpd6/datax2/FRB121102/BLP07', \
+'/mnt_blpd5/datax/FRB121102/BLP10', '/mnt_blpd5/datax/FRB121102/BLP11', \
+'/mnt_blpd5/datax/FRB121102/BLP12', '/mnt_blpd5/datax/FRB121102/BLP13', \
+'/mnt_blpd5/datax/FRB121102/BLP14', '/mnt_blpd5/datax/FRB121102/BLP15', \
+'/mnt_blpd5/datax/FRB121102/BLP16', '/mnt_blpd5/datax/FRB121102/BLP17',\
+'/mnt_blpd5/datax2/FRB121102/BLP20', '/mnt_blpd5/datax2/FRB121102/BLP21', \
+'/mnt_blpd5/datax2/FRB121102/BLP22', '/mnt_blpd5/datax2/FRB121102/BLP23', \
+'/mnt_blpd5/datax2/FRB121102/BLP24', '/mnt_blpd5/datax2/FRB121102/BLP25', \
+'/mnt_blpd5/datax2/FRB121102/BLP26', '/mnt_blpd5/datax2/FRB121102/BLP27', \
+'/mnt_blpd6/datax/FRB121102/BLP30', '/mnt_blpd6/datax/FRB121102/BLP31',\
+'/mnt_blpd6/datax/FRB121102/BLP32', '/mnt_blpd6/datax/FRB121102/BLP33', \
+'/mnt_blpd6/datax/FRB121102/BLP34', '/mnt_blpd6/datax/FRB121102/BLP35', \
+'/mnt_blpd6/datax/FRB121102/BLP36', '/mnt_blpd6/datax/FRB121102/BLP37']
+
+pathdict = {}
+
+for fil in fil_paths:
+	pathdict[fil] = raw_paths
+
+print(pathdict)
+
+columns = ['.fil_paths', '.raw_paths']
+
+with open ('database.csv', 'w') as f:
+	for key in pathdict.keys():
+		f.write("%s,%s\n"%(key,pathdict[key][1:-1]))
+#with open('database.csv', 'w', newline='') as csvfile: 
+#     fieldnames = ['.fil_path', '.raw_path'] 
+#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames) 
+#     writer.writeheader() 
+#     for fil in fil_paths:
+#     	for raw in raw_paths:
+#	    	 writer.writerow({'.fil_path': fil, '.raw_path': raw})
+	    
+
+
+
+
+
+
+
+
+
+
+
+
+ 
---
+++ pipeline_playground/run_spandak.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+import pandas as pd
+
+#Read in -tentative 'database'- .csv
+filepaths = pd.read_csv("database.csv")
+
+filpaths = filepaths.iloc[:,0]
+
+def run_spandak(filpaths, hidm=2000, lodm=100):
+
+	run_commands = []
+	
+	for fil in filpaths:
+		spandak_run = 'SPANDAK ' + '--fil ' + fil + ' --hidm' + str(hidm) + ' --lodm' + str(lodm) + ' --dorfi ' + '--logs=' + fil[:-4]
+		
+		run_commands.append(spandak_run)
+
+	return run_commands
+
+
+def main():
+	
+	run_commands = run_spandak(filpaths, 'FRB')
+
+	for run in run_commands:
+		os.system(run)
+
+main()
