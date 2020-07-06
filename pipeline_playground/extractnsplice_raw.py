0352502e9b51bd3f53793e109dfe08fc7e36a761
Jakob Faber <jakobtfaber@gmail.com>
2020-07-05 22:11:10 -0700

    Changes to extract script.

--- pipeline_playground/extractnsplice_raw.py
+++ pipeline_playground/extractnsplice_raw.py
@@ -21,60 +21,52 @@ for csv in os.listdir("../pipeline_playground/"):
 	if csv.endswith("4chan.csv"):
 		csvs.append(csv)
 
-#print(csvs)
-#print(rawpaths)
-
-#print(filpaths)
-
-#rawpaths = filepaths.iloc[:,1]
-
-#print(rawpaths[0][1])
 
 def run_extract(filpaths, rawpaths, fieldnames, csvs):
 
 	for csv in csvs:
 		#Read in .csv produced by SPANDAK (on FRB121102 as an example)
 		cands = pd.read_csv(csv)
-		#print(cands)
 
 		#Isolate B-valued candidates (probable without ML)
 		B_vals = cands[cands.iloc[:, :]['Category']=='B'].index.values
-		#print(B_vals)
 
 		#Find filenames
 		files = [[i for i in cands.loc[:, :]['filename']][k] for k in B_vals]
+
 		#Find time constraints
 		#Widths
 		time_widths = [[i for i in cands.loc[:, :]['WIDTH']][k] for k in B_vals]
-		#print(time_widths)
 
 		#Centers
 		parse_center = [[i for i in [j.split('_') for j in cands.iloc[:, :]['PNGFILE']]][k][2] for k in B_vals]
 		time_stamps = [np.float(m[:-3]) for m in parse_center]
-		#print(time_stamps)
 
 		#Limits
 		start_times = [time_stamps[i]-time_widths[i] for i in np.arange(len(B_vals))]
-		#print(start_times)
 		end_times = [time_stamps[i]+time_widths[i] for i in np.arange(len(B_vals))]
-		#print(end_times)
 
-		for fil in files:
-			for B in np.arange(len(B_vals)):
-				for raw in np.arange(len(rawpaths)):
-					extract_run = 'python ' + '../extractor/extract_blocks.py ' + rawpaths[raw] \
-					 + '' + 'blc' + str(fieldnames[raw][3:]) + fil[33:-25] \
+		extract_run_commands = []
+
+		for B in np.arange(len(B_vals)):
+			for raw in np.arange(len(rawpaths)):
+				extract_run = 'python ' + '../extractor/extract_blocks.py ' + rawpaths[raw] \
+					 + '' + 'blc' + str(fieldnames[raw][3:]) + files[B][33:-25] \
 					+ ' ' + str(start_times[B]) + ' ' + str(end_times[B]) \
-					 + ' /datax/scratch/jfaber/SPANDAK_extension/pipeline_playground'
+					 + ' /datax/scratch/jfaber/SPANDAK_extension/pipeline_playground/rawfiles'
+				extract_run_commands.append(extract_run)
 
-					return extract_run, B_vals, rawpaths
+
+	return extract_run_commands
 
 
 
 def main():
 
-	extract_run, B, raw = run_extract(filpaths, rawpaths, fieldnames, csvs)
-	os.system(extract_run)
+	extract_run_commands = run_extract(filpaths, rawpaths, fieldnames, csvs)
+
+	for erc in extract_run_commands:
+		os.system(erc)
 
 main()
 
