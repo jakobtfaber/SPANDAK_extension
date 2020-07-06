6a27692c0b6c02e67fe7e1cb3f8818ffa9a3c77e
Jakob Faber <jakobtfaber@gmail.com>
2020-07-05 19:44:25 -0700

    Edits to run_spandak.py

--- pipeline_playground/run_spandak.py
+++ pipeline_playground/run_spandak.py
@@ -5,7 +5,7 @@ import os
 import pandas as pd
 
 #Read in -tentative 'database'- .csv
-filepaths = pd.read_csv("database.csv")
+filepaths = pd.read_csv("database_test.csv")
 
 filpaths = filepaths.iloc[:,0]
 
@@ -16,27 +16,17 @@ def run_spandak(filpaths, hidm=2000, lodm=100):
 	run_commands = []
 	
 	for fil in filpaths:
-<<<<<<< HEAD
-		spandak_run = 'SPANDAK ' + '--fil ' + fil + ' --hidm ' + str(hidm) + ' --lodm ' + str(lodm) + ' --dorfi ' + '--logs=' + fil[:-4] + '.csv'
-=======
-		spandak_run = 'SPANDAK ' + '--fil ' + fil + ' --hidm ' + str(hidm) + ' --lodm ' + str(lodm) + ' --dorfi ' + '--logs=' + fil[26:-4] + '.csv'
->>>>>>> da66f6e97c873160cc72541b4b2760220a7f5e56
-		
+		spandak_run = 'SPANDAK ' + '--fil ' + fil + ' --hidm ' + str(hidm) + ' --lodm ' + str(lodm) + ' --dorfi ' + '--logs=' + fil[26:-4] + '.csv'	
 		run_commands.append(spandak_run)
 
 	return run_commands
 
 
-def main():
-<<<<<<< HEAD
-	
-=======
+def main():	
 
->>>>>>> da66f6e97c873160cc72541b4b2760220a7f5e56
 	run_commands = run_spandak(filpaths)
 
 	for run in run_commands:
-		#os.system(run)
-		print(run)
+		os.system(run)
 
 main()
