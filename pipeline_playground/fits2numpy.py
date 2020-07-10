a12fe6fdea5cb4fdcd8d91ff9cdbca30b9f372e8
Jakob Faber <jakobtfaber@gmail.com>
2020-07-10 17:08:13 -0400

    Edit fits to numpy converter.

--- pipeline_playground/fits2numpy.py
+++ pipeline_playground/fits2numpy.py
@@ -5,21 +5,20 @@ import psrchive
 import pylab
 import sys
 import os
-from tempfile import TemporaryFile
 
 #sys.path.insert(0, os.path.abspath('/datax/scratch/jfaber/SPANDAK_extension/pipeline_playground/61.4627973333_67.0552026667_fits/'))
 
 def main():
 	#for fits in '*.fits':
-	with open		
-		npar_file = 'pulse_120390656' + '.npy'
-		npar_file = TemporaryFile()
+	npar_file = 'pulse_120390656' + '.npy'
+	with open(npar, 'wb') as npar_file:		
 		arch = psrchive.Archive_load('/datax/scratch/jfaber/SPANDAK_extension/pipeline_playground/61.4627973333_67.0552026667_fits/pulse_120390656.fits')
 		arch.dedisperse()
 		arch.remove_baseline()
 		arch.convert_state('Stokes')
 		data = arch.get_data()
-		np.save(data[:, 0, :, :].mean(0))
+		np.save(npar_file, data[:, 0, :, :].mean(0))
+		print('Array Written...')
 
 main()
 
