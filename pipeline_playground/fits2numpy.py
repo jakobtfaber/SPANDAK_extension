a06f4a553b1bd0c4a7c96f761168aa19e383eff5
Jakob Faber <jakobtfaber@gmail.com>
2020-07-12 12:56:46 -0400

    Edits to fits2numpy.py (old--already improved remotely)

--- pipeline_playground/fits2numpy.py
+++ pipeline_playground/fits2numpy.py
@@ -8,17 +8,17 @@ import os
 
 #sys.path.insert(0, os.path.abspath('/datax/scratch/jfaber/SPANDAK_extension/pipeline_playground/61.4627973333_67.0552026667_fits/'))
 
-def main():
-	#for fits in '*.fits':
-	npar_file = 'pulse_120390656' + '.npy'
-	with open(npar, 'wb') as npar_file:		
-		arch = psrchive.Archive_load('/datax/scratch/jfaber/SPANDAK_extension/pipeline_playground/61.4627973333_67.0552026667_fits/pulse_120390656.fits')
-		arch.dedisperse()
-		arch.remove_baseline()
-		arch.convert_state('Stokes')
-		data = arch.get_data()
-		np.save(npar_file, data[:, 0, :, :].mean(0))
-		print('Array Written...')
+def fits2numpy():
+	for fits in '/datax/scratch/jfaber/fits':
+		npar_file = fits[:-4] + '.npy'
+		with open(npar, 'wb') as npar_file:		
+			arch = psrchive.Archive_load('/datax/scratch/jfaber/SPANDAK_extension/pipeline_playground/61.4627973333_67.0552026667_fits/pulse_120390656.fits')
+			arch.dedisperse()
+			arch.remove_baseline()
+			arch.convert_state('Stokes')
+			data = arch.get_data()
+			np.save(npar_file, data[:, 0, :, :].mean(0))
+			print('Array Written...')
 
-main()
+fits2numpy()
 
